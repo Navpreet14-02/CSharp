e07d8e549a8301b9ffc36709551aaac5e00cfc48
Navpreet14-02 <79501562+Navpreet14-02@users.noreply.github.com>
2023-10-11 18:11:45 +0530

    October_11

---
+++ Oct_11/WorkflowEngine.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oct_11
+{
+
+    public interface IActivity
+    {
+
+        void Execute();
+    }
+
+    public class UploadVideo : IActivity
+    {
+        public void Execute() {
+            Console.WriteLine("Uploading the video to the Cloud ");
+        }
+    }
+
+    public class SendEmail : IActivity
+    {
+        public void Execute()
+        {
+            Console.WriteLine("Sending the Email. ");
+        }
+    }
+
+    public class WorkFlow
+    {
+        private readonly IList<IActivity> _workflow;
+
+        public WorkFlow()
+        {
+            _workflow = new List<IActivity>();
+        }
+
+        public void Add(IActivity activity)
+        {
+           _workflow.Add(activity);
+        }
+
+
+        public IEnumerable<IActivity> GetAll()
+        {
+            return _workflow;
+        }
+
+    }
+
+    internal class WorkflowEngine
+    {
+
+
+        public void Run(WorkFlow wk)
+        {
+            foreach(var activity in wk.GetAll())
+            {
+                activity.Execute();
+            }
+        }
+    }
+}
