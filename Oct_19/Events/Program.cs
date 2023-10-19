c990511170d0d7caa8320302f47837f4648df17c
Navpreet14-02 <79501562+Navpreet14-02@users.noreply.github.com>
2023-10-19 17:34:37 +0530

    Oct_19

---
+++ Oct_19/Events/Program.cs
@@ -0,0 +1,119 @@
+﻿
+
+// ======= Events and Delegates ========
+// Events: A mechanism for communication between objects.
+
+
+
+
+
+using System.Security.Cryptography.X509Certificates;
+public class Video
+{
+        
+    public string Title { get; set; }
+}
+
+
+public class VideoEventArgs : EventArgs
+{
+
+    public Video Video { get; set; }
+
+}
+public class VideoEncoder
+{
+
+    // Steps to publish an Event
+    // 1 - Define a delegate
+
+    public delegate void VideoEncodedEventHandler(object source, VideoEventArgs args); // This defines the signature of the method that should be present
+    // in the subscriber.
+    // Source - Refers to class or object that publishes the event.
+    // Eventargs - If we want to send some additional data with the event we can pass it here.
+
+
+
+
+    // 2 - Define an event based on that delegate
+
+    public event VideoEncodedEventHandler VideoEncoded;
+
+    // Instead of creating our own delegate, C# comes with two event handler delegates: EventHandler and EventHandler<>
+    //public event EventHandler<VideoEventArgs> VideoEncoded;
+    // This line states that when VideoEncoded event is raised then all
+    // methods that follow the delegate VideoEncodedEventHandler in the subscriber classes will run.
+
+    // 3 - Raise the event
+
+    protected virtual void OnVideoEncoded(Video v) // The method that raises the event should be protected and virtual
+    {
+        if(VideoEncoded != null) // this statement checks if there are any subscribers
+        {
+            VideoEncoded(this, new VideoEventArgs() { Video = v});
+        }
+    }
+
+    public void Encode(Video video)
+    {
+        Console.WriteLine("Encoding Video...");
+        Thread.Sleep(2000);
+
+        OnVideoEncoded(video);
+        
+    }
+}
+
+
+public class MailService
+{
+
+    public void OnVideoEncoded(object src, VideoEventArgs args)
+    {
+        Console.WriteLine("Sending Email..."+ args.Video.Title);
+    }
+
+
+}
+
+public class MessageService
+{
+    public void AfterVideoEncoded(object src, VideoEventArgs e)
+    {
+        Console.WriteLine("Sending Message..."+e.Video.Title);
+    }
+
+    public void Send()
+    {
+        Console.WriteLine("Message Sent");
+    }
+}
+
+internal class Program
+{
+
+
+
+    private static void Main(string[] args)
+    {
+        Video video = new Video() { Title = "This is a video" };
+
+        VideoEncoder ve = new VideoEncoder(); // publisher
+        MailService ms = new MailService(); // Subscriber
+        MessageService msg = new MessageService(); // Subscriber
+
+        VideoEventArgs vea = new VideoEventArgs();
+
+        //VideoEncoder.VideoEncodedEventHandler vdobj = new VideoEncoder.VideoEncodedEventHandler(ms.OnVideoEncoded);
+        //vdobj += msg.AfterVideoEncoded;
+
+        ve.VideoEncoded += ms.OnVideoEncoded;
+        ve.VideoEncoded += msg.AfterVideoEncoded;
+
+
+        ve.Encode(video);
+        //ve.VideoEncoded += ms
+
+    }
+
+}
---
+++ Oct_19/Extension_Methods/Program.cs
@@ -0,0 +1,63 @@
+﻿
+
+// =========== EXTENSION METHODS =============
+
+// Allow us to add methods to an exisitng class without
+// - changing its source code, or
+// - creating a new class that inherits from it  
+
+
+// To run an extension method, the class that created it should be in the same namespace
+
+
+
+using System.Runtime.CompilerServices;
+
+public static class StringExtensions
+{
+    public static string Shorten(this string /*this is called a binding parameter */ s,int noOfWords)
+    {
+        //Binding parameters are those parameters which are used to bind the new method with the existing class or structure.It does not take any value when you are calling the extension method
+        //because they are used only for binding not for any other use.
+        //In the parameter list of the extension method binding parameter is always present at the first place if you write binding parameter to second, or third, or any other place rather than first place then the compiler will give an error.
+
+        if(noOfWords < 0) throw new ArgumentOutOfRangeException("number of words should be greater than 0.");
+
+        if (noOfWords == 0) return "";
+
+
+        string[] words = s.Split(' ');
+
+        if (words.Length <= noOfWords) return s;
+
+        return string.Join(" ", words.Take(noOfWords))+"...";
+    
+    }
+}
+
+
+// Important Points:
+// - Extension methods are always defined as a static method, but when they are bound with any class or structure they will convert into non-static methods.
+// - When an extension method is defined with the same name and the signature of the existing method, then the compiler will print the existing method, not the extension method.
+// - It cannot apply to fields, properties, or events.
+// - Multiple binding parameters are not allowed means an extension method only contains a single binding parameter. But you can define one or more normal parameter in the extension method.
+
+
+
+
+
+
+internal class Program
+
+{
+    private static void Main(string[] args)
+    {
+
+
+        string post = "This a very very very very very very long string blahh blahh blahh...";
+
+        var shortenedStr = post.Shorten(4);
+
+        Console.WriteLine(shortenedStr);
+    }
+}
