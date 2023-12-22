a3ac86e65a2f2e5a7e17df3590fe33e1ace49f7a
Navpreet14-02 <79501562+Navpreet14-02@users.noreply.github.com>
2023-12-22 20:56:30 +0530

    Delete BloodGuardianAPI directory

--- BloodGuardianAPI/Models/DTO/BloodDonationHistoryDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BloodGuardianAPI.Models.DTO
-{
-    public class BloodDonationHistoryDTO
-    {
-        public string BankName { get; set; }
-        public string BankAddress { get; set; }
-        public int BloodAmntDonated { get; set; }
-    }
-}
